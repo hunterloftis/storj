a900cd59b8ae1227dda842c4ec0307b91deb1cfb
Hunter Loftis <hunter@hunterloftis.com>
2020-02-06 18:05:49 -0500

    tests for secrets

---
+++ relay/secrets_test.go
@@ -0,0 +1,38 @@
+package relay
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestSecretSanity(t *testing.T) {
+	const n = 1000
+	seen := make([]string, 0, n)
+
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	secrets := NewSecrets(rng)
+
+	for i := 0; i < n; i++ {
+		secret := secrets.String()
+		for _, prev := range seen {
+			if prev == secret {
+				t.Errorf("duplicate secret: %v", prev)
+			}
+		}
+		seen = append(seen, secret)
+	}
+}
+
+func TestSecretConsistency(t *testing.T) {
+	rng := rand.New(rand.NewSource(1))
+	secrets := NewSecrets(rng)
+	seq := []string{"fast-blue-began", "fire-type-here", "better-chance-glad"}
+
+	for _, want := range seq {
+		got := secrets.String()
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
